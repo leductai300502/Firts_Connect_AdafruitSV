dd565fb3f1a50251cbdc9ec2871d34635395af65
leductai300502 <tai.leductai@hcmut.edu.vn>
2024-03-02 16:19:31 +0700

    lab2

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import random
 import time
 import  sys
 from  Adafruit_IO import  MQTTClient
+from simple_ai import *
+# from uart import*
 
 AIO_FEED_IDs = ["sensor1","sensor2","sensor3","button1","button2","ai"]
 AIO_USERNAME = "leductai"
-AIO_KEY = ""
+AIO_KEY = "aio_yfTv42StYhbWDexwpGwmx09bYnAU"
 
 def  connected(client):
     print("Ket noi thanh cong...")
@@ -25,23 +27,6 @@ def  message(client , feed_id , payload):
     # ser.write((str(payload) + "#").encode())
 
 
-# def getPort():
-#     ports = serial.tools.list_ports.comports()
-#     N = len(ports)
-#     commPort = "None"
-#     for i in range(0, N):
-#         port = ports[i]
-#         strPort = str(port)
-#         if "USB Serial Device" in strPort:
-#             splitPort = strPort.split(" ")
-#             commPort = (splitPort[0])
-#     return commPort
-
-# ser = serial.Serial( port=getPort(), baudrate=115200)
-# ser = serial.Serial('COM3', 115200)
-# if not ser.isOpen():
-#     ser.open()
-#     print('com3 is open', ser.isOpen())
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
 client.on_disconnect = disconnected
@@ -50,18 +35,34 @@ client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 counter = 10
+counter_ai = 5
+ai_result = ""
+previous_ai = ""
 while True:
-    counter -= 1
-    if counter <= 0:
-        counter = 10
-        temp = random.randint(0, 70)
-        print("Cap nhat nhiet do:", temp)
-        client.publish("sensor1", temp)
-        humi = random.randint(40, 70)
-        print("Cap nhat do am:", humi)
-        client.publish("sensor2", humi)
-        light = random.randint(200, 400)
-        print("Cap nhat anh sang:", light)
-        client.publish("sensor3", light)
+    # counter -= 1
+    # if counter <= 0:
+    #     counter = 10
+    #     temp = random.randint(0, 70)
+    #     print("Cap nhat nhiet do:", temp)
+    #     client.publish("sensor1", temp)
+    #     humi = random.randint(40, 70)
+    #     print("Cap nhat do am:", humi)
+    #     client.publish("sensor2", humi)
+    #     light = random.randint(200, 400)
+    #     print("Cap nhat anh sang:", light)
+    #     client.publish("sensor3", light)
+
+    counter_ai -= 1
+    if counter_ai <= 0:
+        counter_ai = 5
+        ai_result = image_detector()
+        if ai_result != previous_ai :
+            client.publish("ai" , ai_result)
+            previous_ai = ai_result
+        print("AI output : ", ai_result)
+    
+    # readSerial(client)
+
+
     time.sleep(1)
     pass
